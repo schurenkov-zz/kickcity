12d96dd06608e9eb314cec09859128d58888b0f2
SILENTIUM <mr.schurenkov@gmail.com>
2018-07-12 23:00:25 +0300

    fix

--- src/api/main-api.js
+++ src/api/main-api.js
@@ -4,22 +4,22 @@ import qs from 'qs';
 
 
 export function getUsers(){
-  return axios.get('http://5b3b45c6e7659e001496949c.mockapi.io/api/db/users')
+  return axios.get('https://5b3b45c6e7659e001496949c.mockapi.io/api/db/users')
               .then(result=> { return result.data })
 }
 
 
 export const addUser = (user) => {
-  return axios.post('http://5b3b45c6e7659e001496949c.mockapi.io/api/db/users', qs.stringify({name: user.name, email: user.email}) )
+  return axios.post('https://5b3b45c6e7659e001496949c.mockapi.io/api/db/users', qs.stringify({name: user.name, email: user.email}) )
               .then(result=> { return result.data })
 }
 
 export const sendUser = (id) => {
-  return axios.put('http://5b3b45c6e7659e001496949c.mockapi.io/api/db/users/'+ id, qs.stringify({status: true}))
+  return axios.put('https://5b3b45c6e7659e001496949c.mockapi.io/api/db/users/'+ id, qs.stringify({status: true}))
               .then(result=> { return result.data })
 }
 
 export const deleteUsers = (id) => {
-  return axios.delete('http://5b3b45c6e7659e001496949c.mockapi.io/api/db/users/' + id)
+  return axios.delete('https://5b3b45c6e7659e001496949c.mockapi.io/api/db/users/' + id)
               .then(result=> { return result.data })
 }
